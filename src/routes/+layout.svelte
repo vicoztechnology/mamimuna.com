560cf653e3fbe9de4998efe974280e0675a85c28
Tofik Hidayat <vicoztechnology@gmail.com>
2026-09-14 10:48:07 +0700

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	import { page, navigating } from '$app/state';
 	import logo from '$lib/assets/logo40.png';
 	// import function to register Swiper custom elements
-	
+
 	import { preparePageTransition } from '$lib/page-transiton';
 
 	preparePageTransition();
@@ -20,12 +20,19 @@
 >
 	{#if page.url.pathname === '/'}
 		<a href="/" class="ml-2" title="tombol menu">
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" class="fill-on-background dark:fill-dark-on-background"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				height="24px"
+				viewBox="0 -960 960 960"
+				width="24px"
+				class="fill-on-background dark:fill-dark-on-background"
+				><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg
+			>
 			<!--<img src={logo} alt="logo mamimuna" />-->
 		</a>
 	{/if}
 
-	<form class="flex items-center rounded-full w-full ml-2 mr-2" data-sveltekit-keepfocus>
+	<form class="flex items-center rounded-full w-full mr-5" data-sveltekit-keepfocus>
 		<label for="voice-search" class="sr-only">Search</label>
 
 		<div class="relative w-full rounded-full">
@@ -36,7 +43,7 @@
 						height="24px"
 						viewBox="0 -960 960 960"
 						width="24px"
-						class="fill-on-background dark:fill-dark-on-background"
+						class="fill-on-background dark:fill-dark-on-background ml-3"
 						><path
 							d="M380.77-335.39q-102.46 0-173.54-71.07-71.07-71.08-71.07-173.54t71.07-173.54q71.08-71.07 173.54-71.07t173.54 71.07q71.07 71.08 71.07 173.54 0 42.85-14.38 81.85-14.39 39-38.39 67.84l230.16 230.16q8.31 8.3 8.5 20.88.19 12.58-8.5 21.27t-21.08 8.69q-12.38 0-21.07-8.69L530.46-388.16q-30 24.77-69 38.77-39 14-80.69 14Zm0-59.99q77.31 0 130.96-53.66 53.66-53.65 53.66-130.96t-53.66-130.96q-53.65-53.66-130.96-53.66t-130.96 53.66Q196.15-657.31 196.15-580t53.66 130.96q53.65 53.66 130.96 53.66Z"
 						/></svg
@@ -48,7 +55,7 @@
 							height="24px"
 							viewBox="0 -960 960 960"
 							width="24px"
-							class="fill-on-background dark:fill-dark-on-background"
+							class="fill-on-background dark:fill-dark-on-background ml-2"
 							><path
 								d="m294.92-450 206.77 206.77q8.92 8.92 8.81 20.88-.12 11.96-9.42 21.27-9.31 8.69-21.08 9-11.77.31-21.08-9L205.31-454.69q-5.62-5.62-7.92-11.85-2.31-6.23-2.31-13.46t2.31-13.46q2.3-6.23 7.92-11.85l253.61-253.61q8.31-8.31 20.58-8.5 12.27-.19 21.58 8.5 9.3 9.31 9.3 21.38 0 12.08-9.3 21.39L294.92-510H750q12.77 0 21.38 8.62Q780-492.77 780-480t-8.62 21.38Q762.77-450 750-450H294.92Z"
 							/></svg
@@ -58,9 +65,10 @@
 			</div>
 			<a href="/cari" aria-label="cari">
 				<input
+				
 					type="text"
 					id="voice-search"
-					class=" w-full dark:text-dark-on-surface-variant dark:bg-dark-surface-container-high border-none focus:outline-none focus:ring-0 rounded-full block ps-9 pe-3 py-2.5 bg-surface-container-high text-on-surface-variant text-base rounded-base placeholder:text-body"
+					class=" pl-12 w-full dark:text-dark-on-surface-variant dark:bg-dark-surface-container-high border-none focus:outline-none focus:ring-0 rounded-full block ps-9 pe-3 py-2.5 bg-surface-container-high text-on-surface-variant text-base rounded-base placeholder:text-body ml-3"
 					placeholder="cari..."
 					required
 				/>
@@ -85,7 +93,7 @@
 						height="24px"
 						viewBox="0 -960 960 960"
 						width="24px"
-						fill="#1f1f1f"
+						class="fill-on-background dark:fill-dark-on-background"
 						><path
 							d="M480-437.85 277.08-234.92q-8.31 8.3-20.89 8.5-12.57.19-21.27-8.5-8.69-8.7-8.69-21.08 0-12.38 8.69-21.08L437.85-480 234.92-682.92q-8.3-8.31-8.5-20.89-.19-12.57 8.5-21.27 8.7-8.69 21.08-8.69 12.38 0 21.08 8.69L480-522.15l202.92-202.93q8.31-8.3 20.89-8.5 12.57-.19 21.27 8.5 8.69 8.7 8.69 21.08 0 12.38-8.69 21.08L522.15-480l202.93 202.92q8.3 8.31 8.5 20.89.19 12.57-8.5 21.27-8.7 8.69-21.08 8.69-12.38 0-21.08-8.69L480-437.85Z"
 						/></svg
